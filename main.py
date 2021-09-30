88901df2241179680f6dd41798e53df4f312e8dc
slash44 <lrwcoding@gmail.com>
2021-09-30 20:18:04 +0200

    added boilder code

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pygame
+import math
+import sys
+
+pygame.init()
+fps = 60
+clock = pygame.time.Clock()
+game_running = True
+
+
 def main():
-    pass
+
+    # Create the main game loop
+    while game_running:
+
+        # Here goes the boiler code.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == '__main__':
